8f194b0987edac2e9bf07afdab20cc04877764f2
charan220 <raavicharan33@gmail.com>
2021-03-17 05:25:18 +0530

    [Charan]Add

---
+++ SSIS/SsisSampleProject/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using AspNetCore.Reporting;
+using CoreRDLCReport.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreRDLCReport.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public HomeController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment= webHostEnvironment;
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Print()
+        {
+            var dt = new DataTable();
+            dt = GetEmployeeList();
+            string mimetype = "";
+            int extension = 1;
+            var path = $"{this._webHostEnvironment.WebRootPath}\\Reports\\rptEmployee.rdlc";
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            parameters.Add("prm", "RDLC Report");
+            LocalReport localReport = new LocalReport(path);
+            localReport.AddDataSource("dsEmployee", dt);
+            var result = localReport.Execute(RenderType.Pdf, extension, parameters, mimetype);
+            return File(result.MainStream, "application/pdf");
+        }
+
+        private DataTable GetEmployeeList()
+        {
+            var dt = new DataTable();
+            dt.Columns.Add("EmpId");
+            dt.Columns.Add("EmpName");
+            dt.Columns.Add("Department");
+            dt.Columns.Add("Designation");
+            DataRow row;
+            for(int i = 101; i <= 120; i++)
+            {
+                row = dt.NewRow();
+                row["EmpId"] = i;
+                row["EmpName"] = "mr .charan" + i;
+
+                row["Department"] = "IT";
+                row["Designation"] = "Software engineer";
+                dt.Rows.Add(row);
+            }
+            return dt;
+        }
+    }
+}
